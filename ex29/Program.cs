949beba7329b2145283f891044ccba9a20646769
yury <yuriy.t87@gmail.com>
2022-08-02 22:24:38 +0300

    ex29 изменена на рандом

--- ex29/Program.cs
+++ ex29/Program.cs
@@ -7,7 +7,51 @@ namespace ex29
         static void Main(string[] args)
         {
             //Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
-            
+
+            var arr = GetRandomArray(8);
+            WriteArrayToConsole(arr);
+
+            //WriteArrayFromUserInput();
+
+        }
+
+
+        static int[] GetRandomArray(int arrayLength)
+        {
+            Random rnd = new Random();
+            int[] arr = new int[arrayLength];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next();
+            }
+            return arr;
+        }
+
+        static void WriteArrayToConsole(int[] array)
+        {
+            Console.Clear();
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+                if (i != array.Length - 1)
+                    Console.Write(",");
+            }
+
+
+            Console.Write(" -> [");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+                if (i != array.Length - 1)
+                    Console.Write(",");
+            }
+            Console.WriteLine("]");
+        }
+
+
+        static void WriteArrayFromUserInput()
+        {
             while (true)
             {
                 Console.Write("Введите несколько чисел через запятую: ");
@@ -21,8 +65,9 @@ namespace ex29
                 }
                 Console.WriteLine("Неверный ввод!");
             }
-        }
 
+
+        }
 
 
         static int[] GetArray(string str)
